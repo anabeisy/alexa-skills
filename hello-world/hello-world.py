c6b1d927a9ef0fde3490fbf1fcf2fe0696a86cfa
AB Cruz <abcruz@ABs-MacBook-Air.local>
2019-09-27 15:47:02 -0400

    add hello world lambda

---
+++ hello-world/hello-world.py
@@ -0,0 +1,82 @@
+import json
+
+def lambda_handler(event, context):
+    """ Route the incoming request based on type (LaunchRequest, IntentRequest,
+    etc.)
+    """
+    
+    if event['request']['type'] == "LaunchRequest":
+        return get_welcome_response()
+    elif event['request']['type'] == "IntentRequest":
+        return on_intent(event['request'], event['session'])
+   
+def on_intent(intent_request, session):
+    """ Called when the user specifies an intent for this skill """
+
+    intent = intent_request['intent']
+    intent_name = intent_request['intent']['name']
+
+    if intent_name == "GreetHelloWorld":
+        return get_greeting_response() 
+    else:
+        raise ValueError("Invalid intent")
+        
+def get_greeting_response():
+    """ If we wanted to initialize the session to have some attributes we could
+    add those here
+    """
+
+    session_attributes = {}
+    card_title = "Welcome"
+    speech_output = "Hello World!"
+    
+    # If the user either does not reply to the welcome message or says something
+    # that is not understood, they will be prompted again with this text.
+    reprompt_text = None
+    should_end_session = True
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+def get_welcome_response():
+    """ If we wanted to initialize the session to have some attributes we could
+    add those here
+    """
+
+    session_attributes = {}
+    card_title = "Welcome"
+    speech_output = "Welcome to Hello World Greeter!"
+    
+    # If the user either does not reply to the welcome message or says something
+    # that is not understood, they will be prompted again with this text.
+    reprompt_text = "Please tell me your favorite color by saying, " \
+                    "my favorite color is red."
+    should_end_session = False
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+        
+def build_response(session_attributes, speechlet_response):
+    return {
+        'version': '1.0',
+        'sessionAttributes': session_attributes,
+        'response': speechlet_response
+    }
+
+def build_speechlet_response(title, output, reprompt_text, should_end_session):
+    return {
+        'outputSpeech': {
+            'type': 'PlainText',
+            'text': output
+        },
+        'card': {
+            'type': 'Simple',
+            'title': 'SessionSpeechlet - ' + title,
+            'content': 'SessionSpeechlet - ' + output
+        },
+        'reprompt': {
+            'outputSpeech': {
+                'type': 'PlainText',
+                'text': reprompt_text
+            }
+        },
+        'shouldEndSession': should_end_session
+    }

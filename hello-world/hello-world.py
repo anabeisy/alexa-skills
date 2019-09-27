428cd2db72defce57dd8ed64b066baa122698f74
AB Cruz <abcruz@ABs-MacBook-Air.local>
2019-09-27 16:08:59 -0400

    clean up functions

--- hello-world/hello-world.py
+++ hello-world/hello-world.py
@@ -1,45 +1,18 @@
 import json
 
 def lambda_handler(event, context):
-    """ Route the incoming request based on type (LaunchRequest, IntentRequest,
-    etc.)
+    """ 
+        Route the incoming request based on type (LaunchRequest and IntentRequest)
     """
     
     if event['request']['type'] == "LaunchRequest":
         return get_welcome_response()
     elif event['request']['type'] == "IntentRequest":
         return on_intent(event['request'], event['session'])
-   
-def on_intent(intent_request, session):
-    """ Called when the user specifies an intent for this skill """
-
-    intent = intent_request['intent']
-    intent_name = intent_request['intent']['name']
-
-    if intent_name == "GreetHelloWorld":
-        return get_greeting_response() 
-    else:
-        raise ValueError("Invalid intent")
-        
-def get_greeting_response():
-    """ If we wanted to initialize the session to have some attributes we could
-    add those here
-    """
-
-    session_attributes = {}
-    card_title = "Welcome"
-    speech_output = "Hello World!"
-    
-    # If the user either does not reply to the welcome message or says something
-    # that is not understood, they will be prompted again with this text.
-    reprompt_text = None
-    should_end_session = True
-    return build_response(session_attributes, build_speechlet_response(
-        card_title, speech_output, reprompt_text, should_end_session))
 
 def get_welcome_response():
-    """ If we wanted to initialize the session to have some attributes we could
-    add those here
+    """ 
+        Greet the user(s) when the skill is invoked
     """
 
     session_attributes = {}
@@ -48,13 +21,43 @@ def get_welcome_response():
     
     # If the user either does not reply to the welcome message or says something
     # that is not understood, they will be prompted again with this text.
-    reprompt_text = "Please tell me your favorite color by saying, " \
-                    "my favorite color is red."
+    reprompt_text = "Sorry, I didn't get that. Welcome to Hello World Greeter!"
     should_end_session = False
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
+
+def on_intent(intent_request, session):
+    """ 
+        Called when the user specifies an intent for this skill 
+    """
+
+    intent = intent_request['intent']
+    intent_name = intent_request['intent']['name']
+
+    if intent_name == "GreetHelloWorld":
+        return get_greeting_response() 
+    else:
+        raise ValueError("Invalid intent")
+
+def get_greeting_response():
+    """ 
+        The GreetHelloWorld intent has been invoked. Let's greet the users! 
+    """
+
+    session_attributes = {}
+    card_title = "Welcome"
+    speech_output = "Hello World!"
+    
+    reprompt_text = None
+    should_end_session = True
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
         
 def build_response(session_attributes, speechlet_response):
+    """ 
+        Function used to format response in a way that Alexa can understand 
+    """
     return {
         'version': '1.0',
         'sessionAttributes': session_attributes,
@@ -62,6 +65,9 @@ def build_response(session_attributes, speechlet_response):
     }
 
 def build_speechlet_response(title, output, reprompt_text, should_end_session):
+    """ 
+        Function used to format response in a way that Alexa can understand 
+    """
     return {
         'outputSpeech': {
             'type': 'PlainText',
